296bc1f70be038d12221eec4c553bdc99dd40230
rchouhani <rchouhani@me.com>
2025-12-17 16:31:14 +0100

    REFACTO

--- app/index.tsx
+++ app/index.tsx
@@ -1,21 +1,25 @@
-import React, { useState } from 'react';
-import { StyleSheet, View } from 'react-native';
-import Tabbar from './components/Tabbar';
-import MapScreen from './screens/MapScreen';
+import React, { useState } from "react";
+import { StyleSheet, View } from "react-native";
+import "react-native-gesture-handler";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+
+import Tabbar from "./components/Tabbar";
+import MapScreen from "./screens/MapScreen";
 
 export default function App() {
   const [activeTab, setActiveTab] = useState("home");
 
   return (
-    <View style={styles.container}>
-      <MapScreen />
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <View style={styles.container}>
+        <MapScreen />
 
-      <Tabbar active={activeTab} onChange={setActiveTab} />
-    </View>
+        <Tabbar active={activeTab} onChange={setActiveTab} />
+      </View>
+    </GestureHandlerRootView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: { flex: 1 }
+  container: { flex: 1 },
 });
-
